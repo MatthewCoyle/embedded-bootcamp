61ce9e97ab3e4986daa92c7ae0f35a0c56ebff65
Matthew Coyle <matthewcoyle18@gmail.com>
2026-07-08 13:46:54 -0700

    Refactor engineering toolkit into functions

--- Week01/EngineeringToolkit/main.c
+++ Week01/EngineeringToolkit/main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-int main(void)
+void printMenu(void)
 {
-    int choice;
-
     printf("=====================================\n");
     printf("      Engineering Toolkit\n");
     printf("=====================================\n\n");
@@ -11,58 +9,80 @@ int main(void)
     printf("1. Say Hello\n");
     printf("2. Calculator\n");
     printf("3. Exit\n\n");
+}
 
-    printf("Choose an option: ");
-    scanf("%d", &choice);
+void sayHello(void)
+{
+    printf("\nHello, Engineer!\n");
+}
 
-    if (choice == 1)
-    {
-        printf("\nHello, Engineer!\n");
-    }
-    else if (choice == 2)
-    {
+void calculator(void)
+{
     int num1, num2;
     char op;
 
     printf("\nEnter your calculation : ");
     scanf("%d %c %d", &num1, &op, &num2);
         
-        if (op == '+')
+    if (op == '+')
+    {
+        printf("\nResult: %d\n", num1 + num2);
+    }
+    else if (op == '-')
+    {
+        printf("\nResult: %d\n", num1 - num2);
+    }
+    else if (op == '*')
+    {
+        printf("\nResult: %d\n", num1 * num2);
+    }
+    else if (op == '/')
+    {
+        if (num2 != 0)
         {
-            printf("\nResult: %d\n", num1 + num2);
-        }
-        else if (op == '-')
-        {
-            printf("\nResult: %d\n", num1 - num2);
-        }
-        else if (op == '*')
-        {
-            printf("\nResult: %d\n", num1 * num2);
-        }
-        else if (op == '/')
-        {
-            if (num2 != 0)
-            {
-                printf("\nResult: %.2f\n", (float)num1 / num2);
-            }
-            else
-            {
-                printf("\nError: Division by zero is not allowed.\n");
-            }
+            printf("\nResult: %.2f\n", (float)num1 / num2);
         }
         else
         {
-            printf("\nInvalid operator. Please use +, -, *, or /.\n");
+            printf("\nError: Division by zero is not allowed.\n");
         }
-    }
-    else if (choice == 3)
-    {
-        printf("\nGoodbye!\n");
     }
     else
     {
-        printf("\nInvalid option.\n");
+        printf("\nInvalid operator. Please use +, -, *, or /.\n");
     }
+        
+}
+
+int main(void)
+{
+    int choice;
+
+    do
+    {
+        printMenu();
+
+        printf("Choose an option: ");
+        scanf("%d", &choice);
+
+        if (choice == 1)
+        {
+            sayHello();  
+        }
+        else if (choice == 2)
+        {
+            calculator();
+        }
+        else if (choice == 3)
+        {
+            printf("\nGoodbye!\n");
+        }
+        else
+        {
+            printf("\nInvalid option.\n");
+        }
+
+    } while (choice != 3);
 
     return 0;
 }
